0475f55c93a0bad32e2c3950a291b88d83a503e4
Gurdeep-Code <s.gurdeep9986@gmail.com>
2026-04-28 00:36:22 +0530

    Update Vite configuration to set base path to root for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  base: "/admission-chat",
+  base: "/",
   server: {
     host: "::",
     port: 3000,
